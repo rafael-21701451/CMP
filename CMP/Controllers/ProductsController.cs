23c0058174269fa0952f1c9b01714939e4e277f5
Rafael Lemos <rafael_lemos@live.com.pt>
2020-04-03 16:45:53 +0100

    Add project files.

---
+++ CMP/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CMP.Models;
+
+namespace CMP.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ArtigosBlog()
+        {
+            return View();
+        }
+
+        public IActionResult ArtigoBlogA()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
