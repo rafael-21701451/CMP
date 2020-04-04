ef1f53e78504cee10a19d5ca2341df609e2a9bd7
Rafael Lemos <rafael_lemos@live.com.pt>
2020-04-04 17:26:26 +0100

    Newsletter implementado

--- CMP/Controllers/HomeController.cs
+++ CMP/Controllers/HomeController.cs
@@ -5,14 +5,83 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CMP.Models;
+using System.Data.SqlClient;
+using System.Net.Mail;
+using System.Net;
 
 namespace CMP.Controllers
 {
     public class HomeController : Controller
     {
+        public object Configuration { get; private set; }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Account account)
+        {
+            String email = "";
+            String nomeUser = "";
+            bool jaSubscrito = false;
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=SGPAV;Trusted_Connection=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Select * From AppLogin Where Email='{account.email}'";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            email = Convert.ToString(dataReader["Email"]);
+                            nomeUser = Convert.ToString(dataReader["Login"]);
+                           //jaSubscrito = Convert.ToBoolean(dataReader["subscricao"]);
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            if (email.Equals(""))
+            {
+                ModelState.AddModelError("", "Email Obrigatório");
+                return View();
+
+            }
+            else if (jaSubscrito)
+            {
+                return View();
+            }
+            else
+            {
+                SmtpClient client = new SmtpClient("smtp.live.com");
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential("rafael_lemos@live.com.pt", "Sporting7675.");
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress("rafael_lemos@live.com.pt");
+                mailMessage.To.Add(email);
+                mailMessage.IsBodyHtml = true;
+                mailMessage.Body = "<html>"
+                                  + "<body>"
+                                  + "<div>"
+                                  + $"<p>Olá {nomeUser},</p>"
+                                  + "</div>"
+                                  + "<div>"
+                                  + "<p>Obrigado por subscrever à nossa newsletter.</p>"
+                                  + "</div>"
+                                  + "<div>"
+                                  + $"<p>Irá recber todas as novidades mais recentes da plataforma!</p>"
+                                  + "</div>"
+                                  + "</body>"
+                                  + "</html>";
+                mailMessage.Subject = "Subscrição Newsletter";
+                client.Send(mailMessage);
+                return View();
+            }
         }
 
 
---
+++ CMP/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMP.Models
+{
+    public class Account
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+    }
+}
