9ce99075e0d8336be3a76db2448b582fff46ae4c
Rafael Lemos <rafael_lemos@live.com.pt>
2020-04-05 18:23:12 +0100

    LoginRegistarView

---
+++ CMP/Models/LoginRegistarView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMP.Models
+{
+    public class LoginRegistarView
+    {
+        public string email { get; set; }
+
+        public string password { get; set; }
+
+        public string nome { get; set; }
+
+        public string morada { get; set; }
+
+    }
+}
