1568b0021b21a88605387e286a634627687b422a
Rafael Lemos <rafael_lemos@live.com.pt>
2020-04-05 18:16:02 +0100

    Criação de compra e briefing

--- CMP/Controllers/ProductsController.cs
+++ CMP/Controllers/ProductsController.cs
@@ -5,11 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CMP.Models;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
 
 namespace CMP.Controllers
 {
     public class ProductsController : Controller
     {
+
+        private readonly IConfiguration _configuration;
+
+        public ProductsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -22,6 +32,49 @@ namespace CMP.Controllers
 
         public IActionResult ArtigoBlogA()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ArtigoBlogA(Briefing briefing)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Insert Into Compra (sub_total, iva, total, estado_id, cliente_id) Values ('{200}','{23}','{200 + (200*0.23)}','{1}','{1}')";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.CommandType = System.Data.CommandType.Text;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+                sql = $"Insert Into Produto_Compra (valor_desconto, compra_id, produto_id) Values ('{0}','{1}','{1}')";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.CommandType = System.Data.CommandType.Text;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+                sql = $"Insert Into Briefing (empresa, setor, historia_empresa,objetivo_negocio, estrategia, produtos_comercializados, marca, imagem_corporativa" +
+                    $",posicionamento,publico_alvo,concorrentes,objetivos,resultados_esperados,permissas,restricoes,data_entrega,cronograma_1,cronograma_2,cronograma_3" +
+                    $",linha_seguir,tom_voz,tipo_letra,cor,aceite,produto_compra_id) " +
+                    $"Values ('{briefing.empresa}','{briefing.setor}','{briefing.historia_empresa}','{briefing.objetivo_negocio}','{briefing.estrategia}','{briefing.produtos_comercializados}','{briefing.marca}','{briefing.imagem_corporativa}'" +
+                    $",'{briefing.posicionamento}','{briefing.publico_alvo}','{briefing.concorrentes}','{briefing.objetivos}','{briefing.resultados_esperados}','{briefing.permissas}','{briefing.restricoes}','{string.Format("{0:yyyy-MM-dd}", briefing.data_entrega)}','{string.Format("{0:yyyy-MM-dd}", briefing.cronograma_1)}','{string.Format("{0:yyyy-MM-dd}", briefing.cronograma_2)}','{string.Format("{0:yyyy-MM-dd}", briefing.cronograma_3)}'" +
+                    $",'{briefing.linha_seguir}','{briefing.tom_voz}','{briefing.tipo_letra}','{briefing.cor}','false','{1}')";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.CommandType = System.Data.CommandType.Text;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+
+            }
             return View();
         }
 
---
+++ CMP/Models/Briefing.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMP.Models
+{
+    public class Briefing
+    {
+        public int id { get; set; }
+
+        public string empresa { get; set; }
+
+        public string setor { get; set; }
+
+        public string historia_empresa { get; set; }
+
+        public string objetivo_negocio { get; set; }
+
+        public string estrategia { get; set; }
+
+        public string produtos_comercializados { get; set; }
+
+        public string marca { get; set; }
+
+        public string imagem_corporativa { get; set; }
+
+        public string posicionamento { get; set; }
+
+        public string publico_alvo { get; set; }
+
+        public string concorrentes { get; set; }
+
+        public string objetivos { get; set; }
+
+        public string resultados_esperados { get; set; }
+
+        public string permissas { get; set; }
+
+        public string restricoes { get; set; }
+
+        public DateTime data_entrega { get; set; }
+
+        public DateTime cronograma_1 { get; set; }
+
+        public DateTime cronograma_2 { get; set; }
+
+        public DateTime cronograma_3 { get; set; }
+
+        public string linha_seguir { get; set; }
+
+        public string tom_voz { get; set; }
+
+        public string tipo_letra { get; set; }
+
+        public string cor { get; set; }
+
+    }
+}
