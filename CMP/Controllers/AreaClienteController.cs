541f5e8162b0bbee667f2d83c56221b136268664
Rafael Lemos <rafael_lemos@live.com.pt>
2020-04-07 16:55:20 +0100

    Login Feito

--- CMP/Controllers/AreaClienteController.cs
+++ CMP/Controllers/AreaClienteController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CMP.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CMP.Controllers
 {
+    [Authorize]
     public class AreaClienteController : Controller
     {
         public IActionResult Index()
