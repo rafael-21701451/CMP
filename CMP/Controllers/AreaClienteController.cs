4a9022cced3195658f315f33a1412e552f4cae52
Rafael Lemos <rafael_lemos@live.com.pt>
2020-04-15 17:28:20 +0100

    aeFf

--- CMP/Controllers/AreaClienteController.cs
+++ CMP/Controllers/AreaClienteController.cs
@@ -8,14 +8,52 @@ using CMP.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace CMP.Controllers
 {
     public class AreaClienteController : Controller
     {
+
+        private readonly IConfiguration _configuration;
+
+        public AreaClienteController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult MinhasEncomendas()
+        {
+            List<CompraPerfil> compras = new List<CompraPerfil>();
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"SELECT * FROM Compra";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            CompraPerfil compra = new CompraPerfil();
+                            compra.id = Convert.ToInt32(dataReader["id"]);
+                            compra.data = Convert.ToInt32(dataReader["sub_total"]);
+                            compra.total = Convert.ToInt32(dataReader["total"]);
+                            compra.estado = Convert.ToInt32(dataReader["iva"]);
+                            compras.Add(compra);
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            return View(compras);
         }
 
         public async Task<ActionResult> Logout()
---
+++ CMP/Models/CompraPerfil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMP.Models
+{
+    public class CompraPerfil
+    {
+        public int id { get; set; }
+        public string estado { get; set; }
+        public string data { get; set; }
+        public double total { get; set; }
+    }
+}
