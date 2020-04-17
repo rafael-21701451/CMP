d601c0a5a32c060debde4a83090b7638707d2555
Rafael Lemos <rafael_lemos@live.com.pt>
2020-04-17 15:11:15 +0100

    Falta retangulos

--- CMP/Controllers/AreaClienteController.cs
+++ CMP/Controllers/AreaClienteController.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using System.Security.Cryptography;
+using System.Security.Claims;
 
 namespace CMP.Controllers
 {
@@ -25,12 +27,107 @@ namespace CMP.Controllers
 
         public IActionResult EditarDados(int id)
         {
-            return View();
+            if (id != Convert.ToInt32(User.Claims.ElementAt(2).Value))
+            {
+                return RedirectToAction("DadosPessoais");
+            }
+            else
+            {
+                EditarPerfilView dados = new EditarPerfilView();
+                dados.id = id;
+                DadosPerfil d = getAccountDetails(Convert.ToInt32(this.User.Claims.ElementAt(2).Value));
+                dados.username = d.username;
+                dados.email = d.email;
+                int idCliente = getidCliente(Convert.ToInt32(this.User.Claims.ElementAt(2).Value));
+                dados.nome = getClientName(idCliente);
+                return View(dados);
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditarDados(EditarPerfilView dados)
+        {
+            if (ModelState.IsValid)
+            {
+                bool valid = true;
+                DadosPerfil d = getAccountDetails(Convert.ToInt32(this.User.Claims.ElementAt(2).Value));
+                if (!verificarUsername(dados.username) && !d.username.Equals(dados.username))
+                {
+                    valid = false;
+                    ModelState.AddModelError("username", "Username já existe");
+                }
+                if (!verificarEmail(dados.email) && !d.email.Equals(dados.email))
+                {
+                    valid = false;
+                    ModelState.AddModelError("email", "Email já existe");
+                }
+                if (!dados.password.Equals(dados.passwordCmf))
+                {
+                    valid = false;
+                    ModelState.AddModelError("passwordCmf", "Passwords Não Coincidem");
+                }
+                if (valid == false)
+                {
+                    return View(dados);
+                }
+                int idCliente = getidCliente(Convert.ToInt32(this.User.Claims.ElementAt(2).Value));
+                AtualizarPerfil(dados, Convert.ToInt32(this.User.Claims.ElementAt(2).Value), idCliente);
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                var claims = new[] {
+                                new Claim(ClaimTypes.Name, dados.email),//0
+                                new Claim("nome", dados.nome),//1
+                                new Claim("id", Convert.ToString(getIdByEmail(dados.email))),//2
+                            };
+                var authProperties = new AuthenticationProperties
+                {
+                    IsPersistent = true,
+                };
+
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity), authProperties);
+                return RedirectToAction("DadosPessoais");
+            }
+            else
+            {
+                return View(dados);
+            }
         }
 
         public IActionResult VerCompra(int id)
         {
-            return View();
+            CompraView encomenda = new CompraView();
+            encomenda.compra = new Compra();
+            encomenda.compra.id = id;
+            encomenda.compra.total = getPrecoCompraByID(id);
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"SELECT * FROM Compra WHERE id={id}";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                           encomenda.estado = getNomeEstado(Convert.ToInt32(dataReader["estado_id"]));
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            encomenda.fatura = new FinalizarCompraView();
+            encomenda.fatura = getFatura(id);
+            List<Product> ProdutosCompra = getProdutosCompra(id);
+            List<Product> produtos = new List<Product>();
+            foreach (Product p in ProdutosCompra)
+            {
+                produtos.Add(getProductByID(p));
+            }
+            encomenda.produtos = produtos;
+            return View(encomenda);
         }
 
         public IActionResult Index()
@@ -38,7 +135,7 @@ namespace CMP.Controllers
             int idCliente = getidCliente(Convert.ToInt32(this.User.Claims.ElementAt(2).Value));
             UltimaCompra compra = getUltimaCompraByCliente(idCliente);
             List<Product> ProdutosCompra = getProdutosCompra(compra.id);
-            List<Product> produtos= new List<Product>();
+            List<Product> produtos = new List<Product>();
             foreach (Product p in ProdutosCompra)
             {
                 produtos.Add(getProductByID(p));
@@ -101,7 +198,7 @@ namespace CMP.Controllers
                     {
                         while (dataReader.Read())
                         {
-                           foreach(var c in compras)
+                            foreach (var c in compras)
                             {
                                 if (c == Convert.ToInt32(dataReader["compra_id"]))
                                 {
@@ -134,7 +231,7 @@ namespace CMP.Controllers
                         {
                             CompraPerfil compra = new CompraPerfil();
                             compra.id = Convert.ToInt32(dataReader["id"]);
-                            compra.data = getDataCompra( Convert.ToInt32(dataReader["id"]));
+                            compra.data = getDataCompra(Convert.ToInt32(dataReader["id"]));
                             compra.total = Convert.ToInt32(dataReader["total"]);
                             compra.estado = getNomeEstado(Convert.ToInt32(dataReader["estado_id"]));
                             if (!compra.estado.Equals("Por Pagar"))
@@ -199,10 +296,37 @@ namespace CMP.Controllers
             return "";
         }
 
+        public FinalizarCompraView getFatura(int idCompra)
+        {
+            FinalizarCompraView fatura = new FinalizarCompraView();
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"SELECT * FROM Fatura";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            fatura.nome = Convert.ToString(dataReader["nome"]);
+                            fatura.morada = Convert.ToString(dataReader["morada"]);
+                            fatura.nif = Convert.ToString(dataReader["nif"]);
+                            fatura.tlm = Convert.ToString(dataReader["telemovel"]);
+                            fatura.email = Convert.ToString(dataReader["email"]);
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            return fatura;
+        }
+
         public async Task<ActionResult> Logout()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
         }
 
 
@@ -226,7 +350,7 @@ namespace CMP.Controllers
                     {
                         while (dataReader.Read())
                         {
-                            if (Convert.ToInt32(dataReader["estado_id"])!=getEstadoByNome("Por Pagar"))
+                            if (Convert.ToInt32(dataReader["estado_id"]) != getEstadoByNome("Por Pagar"))
                             {
                                 compra.id = Convert.ToInt32(dataReader["id"]);
                                 compra.estado = getEstadoById(Convert.ToInt32(dataReader["estado_id"]));
@@ -423,6 +547,28 @@ namespace CMP.Controllers
             return null;
         }
 
+        public int getPrecoCompraByID(int id)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"SELECT * FROM Compra WHERE id={id}";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                                return Convert.ToInt32(dataReader["total"]);
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            return -1;
+        }
+
         public DadosPerfil getAccountDetails(int id)
         {
             DadosPerfil dados = new DadosPerfil();
@@ -446,5 +592,115 @@ namespace CMP.Controllers
             }
             return dados;
         }
+
+        public bool verificarEmail(String email)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"SELECT * FROM Account";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            if (Convert.ToString(dataReader["email"]).Equals(email))
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            return true;
+        }
+
+        public bool verificarUsername(String user)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"SELECT * FROM Account";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            if (Convert.ToString(dataReader["username"]).Equals(user))
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            return true;
+        }
+
+        public void AtualizarPerfil(EditarPerfilView dados, int idAccount, int idClient)
+        {
+            String password = dados.password;
+            HashAlgorithm hasher;
+            hasher = new SHA256Managed();
+            byte[] mPasswordBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(password);
+            byte[] mPasswordHash = hasher.ComputeHash(mPasswordBytes);
+            password = Convert.ToBase64String(mPasswordHash, 0, mPasswordHash.Length);
+
+            string connectionString = _configuration["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Update Account SET username='{dados.username}', email='{dados.email}', password='{password}' Where id='{idAccount}'";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    
+                }
+
+                sql = $"Update Cliente SET nome='{dados.nome}' Where id='{idClient}'";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+
+                }
+
+
+            }
+        }
+
+        public int getIdByEmail(String email)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"SELECT * FROM Account";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            if (Convert.ToString(dataReader["email"]).Equals(email))
+                            {
+                                return Convert.ToInt32(dataReader["id"]);
+                            }
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            return -1;
+        }
+
     }
 }
