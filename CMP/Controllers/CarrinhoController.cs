c2b1e60b3ec2bfa6f19d3848b96d53401c3d4e43
Rafael Lemos <rafael_lemos@live.com.pt>
2020-04-22 19:19:25 +0100

    Incio get Briefing

--- CMP/Controllers/CarrinhoController.cs
+++ CMP/Controllers/CarrinhoController.cs
@@ -22,6 +22,52 @@ namespace CMP.Controllers
             _configuration = configuration;
         }
 
+        public IActionResult VerBriefing(int idBriefing)
+        {
+            Briefing briefing = new Briefing();
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"SELECT * FROM Briefing Where id={idBriefing}";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            briefing.id = Convert.ToInt32(dataReader["id"]);
+                            briefing.empresa = Convert.ToString(dataReader["empresa"]);
+                            briefing.setor = Convert.ToString(dataReader["setor"]);
+                            briefing.historia_empresa = Convert.ToString(dataReader["historia_empresa"]);
+                            briefing.objetivo_negocio = Convert.ToString(dataReader["objetivo_negocio"]);
+                            briefing.estrategia = Convert.ToString(dataReader["estrategia"]);
+                            briefing.produtos_comercializados = Convert.ToString(dataReader["produtos_comercializados"]);
+                            briefing.marca = Convert.ToString(dataReader["marca"]);
+                            briefing.imagem_corporativa = Convert.ToString(dataReader["imagem_corporativa"]);
+                            briefing.posicionamento = Convert.ToString(dataReader["posicionamento"]);
+                            briefing.publico_alvo = Convert.ToString(dataReader["publico_alvo"]);
+                            briefing.concorrentes = Convert.ToString(dataReader["concorrentes"]);
+                            briefing.objetivos = Convert.ToString(dataReader["objetivos"]);
+                            briefing.resultados_esperados = Convert.ToString(dataReader["resultados_esperados"]);
+                            briefing.permissas = Convert.ToString(dataReader["permissas"]);
+                            briefing.restricoes = Convert.ToString(dataReader["restricoes"]);
+                            briefing.data_entrega = Convert.ToDateTime(dataReader["data_entrega"]);
+                            briefing.cronograma_1 = Convert.ToDateTime(dataReader["cronograma_1"]);
+                            briefing.cronograma_2 = Convert.ToDateTime(dataReader["cronograma_2"]);
+                            briefing.cronograma_3 = Convert.ToDateTime(dataReader["cronograma_3"]);
+                            briefing.linha_seguir = Convert.ToString(dataReader["linha_seguir"]);
+                            briefing.tom_voz = Convert.ToString(dataReader["tom_voz"]);
+                            briefing.tipo_letra = Convert.ToString(dataReader["tipo_letra"]);
+                            briefing.cor = Convert.ToString(dataReader["cor"]);
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            return View(briefing);
+        }
+
        
 
         public IActionResult Pagamento()
