de42ddddece155c350e7ea96a131c4a695aed53e
Bruna <a21703913@alunos.ulht.pt>
2020-06-23 13:14:29 +0100

    fhdishfs

---
+++ CMP/Models/ProjetoPorActribuir.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMP.Models
+{
+    public class ProjetoPorAtribuir
+    {
+        public int id { get; set; }
+        public string produto { get; set; }
+        public string comprador { get; set; }
+        public int idCompra { get; set; }
+    }
+}
