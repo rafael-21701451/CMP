87969ba547973c4cfa04f2b3ce32f8e8c553f246
Bruna <a21703913@alunos.ulht.pt>
2020-06-24 14:29:32 +0100

    Projeto Por Validar

---
+++ CMP/Models/ProjetoPorValidar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMP.Models
+{
+    public class ProjetoPorValidar
+    {
+        public int id { get; set; }
+        public string produto { get; set; }
+        public string produtor { get; set; }
+    }
+}
