79cc2a4827f719e600b00e82f4805c21984a8916
Bruna <a21703913@alunos.ulht.pt>
2020-06-25 22:26:31 +0100

    Mensgens CM

---
+++ CMP/Models/MensagemCliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMP.Models
+{
+    public class MensagemCliente
+    {
+        public int id { get; set; }
+        public String assunto { get; set; }
+        public String remetente { get; set; }
+        public String textoMensagem { get; set; }
+    }
+}
